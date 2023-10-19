40b88ac509336756692cafc1cdd01db7566053ef
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-19 21:54:39 +0900

    feat: View 유저가 입력한 값 올바르지 않으면 에러 출력 로직 구현

--- src/view/View.js
+++ src/view/View.js
@@ -1,5 +1,8 @@
+import { Console } from "@woowacourse/mission-utils";
+
 export default class View {
-  constructor() {
+  constructor(controller) {
+    this.controller = controller;
     this.GAME_START_COMMENT = '숫자 야구 게임을 시작합니다.';
     this.INPUT_NUMBER_COMMENT = '숫자를 입력해주세요 : ';
     this.INPUT_NUMBER_ERROR = '--입력창 오류-- 1부터 9사이의 서로 다른 3개의 숫자를 선택해주세요!!'
@@ -7,5 +10,9 @@ export default class View {
     this.RESTART_INPUT_COMMENT = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n';
     this.RESTART_INPUT_ERROR = '--입력창 오류-- 1(재시작) 또는 2(게임 종료)만 눌러주세요!!';
     this.GAME_OVER_COMMENT = '이용해주셔서 감사합니다. 게임을 종료합니다.';
+  }
+
+  printInputNumberError() {
+    throw new Error(this.INPUT_NUMBER_ERROR);
   }
 }

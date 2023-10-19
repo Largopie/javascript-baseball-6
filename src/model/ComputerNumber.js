4c9f2bf6802b28ed3fbbb6600bad8b6c7c67cd6c
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-19 21:19:12 +0900

    feat: ComputerNumber 컴퓨터가 임의로 선택한 3개의 수 설정 메서드 구현

--- src/model/ComputerNumber.js
+++ src/model/ComputerNumber.js
@@ -1,3 +1,5 @@
+import { Random } from "@woowacourse/mission-utils";
+
 /**
  * 컴퓨터(Model)에 관련된 클래스.
  * 컴퓨터가 임의로 선택한 3개의 수 구현 및 상태 관리
@@ -13,5 +15,9 @@ export default class ComputerNumber {
 
   setNumber(newNumber) {
     this.number = newNumber;
+  }
+
+  chooseRandomNumber() {
+    this.setNumber(Random.pickUniqueNumbersInRange(1,9,3));
   }
 }

5f7cbb0e7dc3fadfc53f821228f3c4ad3d42ca28
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-19 21:26:18 +0900

    feat: Controller constructor 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,3 +1,13 @@
+import ComputerNumber from "../model/ComputerNumber.js";
+import UserInputNumber from "../model/UserInputNumber.js";
+import View from "../view/View.js";
+
 export default class Controller {
-  constructor() {}
+  // 첫 게임 여부에 따라 게임 시작 메시지 출력 여부 구분.
+  constructor(isFirstGame) {
+    this.isFirstGame = isFirstGame;
+    this.view = new View();
+    this.computerNumber = new ComputerNumber();
+    this.userInputNumber = new UserInputNumber();
+  }
 }

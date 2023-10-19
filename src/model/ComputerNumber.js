432a3d54441fac5b85729e8a5d09ad7ffba4d00b
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-19 16:47:32 +0900

    feat: ComputerNumber class 관련 getter, setter 구현

--- src/model/ComputerNumber.js
+++ src/model/ComputerNumber.js
@@ -1,3 +1,13 @@
-export default class computeNumber {
-  constructor() {}
+export default class ComputeNumber {
+  constructor() {
+    this.number = null;
+  }
+
+  getNumber() {
+    return this.number;
+  }
+
+  setNumber(newNumber) {
+    this.number = newNumber;
+  }
 }

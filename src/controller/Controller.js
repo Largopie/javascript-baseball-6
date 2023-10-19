c76983dd38c0cf6eb49d1928f00389b8fda0002a
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-19 17:19:49 +0900

    feat: Controller 클래스 생성자 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,3 +1,11 @@
+import View from '../view/View';
+import ComputerNumber from '../model/ComputerNumber';
+import UserInputNumber from '../model/UserInputNumber';
+
 export default class Controller {
-  constructor() {}
+  constructor() {
+    this.view = new View();
+    this.computerNumber = new ComputerNumber();
+    this.UserInputNumber = new UserInputNumber();
+  }
 }

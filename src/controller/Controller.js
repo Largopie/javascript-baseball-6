240daed92008b6728047f8af9043fb8b2b4c2838
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-19 18:26:58 +0900

    feat: Controller 유저가 입력한 input 값이 유효한 값인지 검증 로직 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,15 +1,22 @@
-import View from '../view/View';
-import ComputerNumber from '../model/ComputerNumber';
-import UserInputNumber from '../model/UserInputNumber';
+import View from '../view/View.js';
+import ComputerNumber from '../model/ComputerNumber.js';
+import UserInputNumber from '../model/UserInputNumber.js';
+import Validation from '../util/Validation.js';
 
 export default class Controller {
   constructor() {
     this.view = new View();
     this.computerNumber = new ComputerNumber();
     this.userInputNumber = new UserInputNumber();
+    this.validation = new Validation();
   }
 
   updateUserInputNumber(inputNumber) {
     this.userInputNumber.setNumber(inputNumber);
+  }
+
+  userInputValidation(inputNumber) {
+    this.validation.checkUserInputValidation(inputNumber);
+    return this.validation.getUserInputValid();
   }
 }

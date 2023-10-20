ad9a23f5cb5cfbb67dcc8220dead8ccc965d9dd7
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-20 17:34:49 +0900

    chore: test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async throwError(input) {
+    const e = new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+    throw e;
+  }
+
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    
+    try {
+      const userInput = await Console.readLineAsync('숫자를 입력해주세요 : ');
+      await this.throwError(userInput);
+      
+    } catch (error) {
+      Console.print(error);
+    }
+  }
 }
 
 export default App;
